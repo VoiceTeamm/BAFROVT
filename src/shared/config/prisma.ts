7c5d7ae61dc4bf801b30b2d9a91f0f621a8f0fbe
VictorCa23 <vector2.cartalvarez@gmail.com>
2026-05-18 14:49:15 -0400

    fix: resolver conflicto en prisma.ts

--- src/shared/config/prisma.ts
+++ src/shared/config/prisma.ts
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-﻿import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
-
-const connectionString = process.env.DATABASE_URL!;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-=======
 import { PrismaClient } from '../../generated/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
@@ -21,13 +6,12 @@ const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    adapter,
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  });
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        adapter,
+        log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+    });
 
 if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
+    globalForPrisma.prisma = prisma;
 }
->>>>>>> 6bc3843351175d8768e9235cae85b31d7e215ba1
