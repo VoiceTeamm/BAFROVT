edd1ee9c0423d41c077385997d102cf39be00e23
paolacrispin <paola.crispin.pc@gmail.com>
2026-05-18 18:14:48 -0400

    fix: stabilize backend local setup

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'prisma/config';
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-    datasource: {
-    },
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: process.env.DATABASE_URL!
+  }
 });
